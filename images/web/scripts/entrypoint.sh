d1ed8607f9af07e04a7e1bab70c149475bf9fe71
Per Macha <macha@univention.de>
2021-03-23 00:00:00 +0100

    feat(web): handle stale pid [iam-issues#87]

--- images/web/scripts/entrypoint.sh
+++ images/web/scripts/entrypoint.sh
@@ -2,7 +2,14 @@
 set -euxo pipefail
 umask 077
 
-/usr/sbin/univention-management-console-web-server "$@"
+PID_PATH=/var/run/umc-web-server.pid
+
+if [[ -f "${PID_PATH}" ]]; then
+  echo "Removing stale pid ${PID_PATH}"
+  rm "${PID_PATH}"
+fi
+
+exec /usr/sbin/univention-management-console-web-server "$@"
 
 exit 0
 
