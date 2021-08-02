19068874f55cf02e9caee1f7f45c03c2eca84334
Per Macha <macha@univention.de>
2021-08-03 00:00:01 +0200

    feat(ci): sync ci-libs, use docker-envs, rename image names [iam-issues#150]

--- ci-build-script.py
+++ ci-build-script.py
@@ -9,7 +9,6 @@ import glob
 import os
 import shutil
 import sys
-import time
 
 # third party
 import sh  # pylint: disable=import-error
@@ -27,92 +26,46 @@ import ci_vars  # noqa: E402; pylint: disable=import-error,wrong-import-position
 import ci_version  # noqa: E402,E501; pylint: disable=import-error,wrong-import-position
 
 # pylint: disable=not-callable
-sh_noout = sh(_cwd=BASE_DIR)
-
-# pylint: disable=not-callable
 sh_out = sh(_out='/dev/stdout', _err='/dev/stderr', _cwd=BASE_DIR)
 
 
 def main(service):
     """The main script builds, labels and pushes"""
 
-    docker_env = ci_vars.get_env_vars(ci_vars.MINIMAL_DOCKER_VARS)
-
-    pull_push_env = ci_vars.get_env_vars(ci_vars.ADDITIONAL_PULL_PUSH_VARS)
-    pull_push_env.update(docker_env)
-
-    compose_env = {
-        'COMPOSE_DOCKER_CLI_BUILD': '0',
-        'CI_PROJECT_URL': 'unset',
-        'CI_PIPELINE_ID': ci_vars.DEFAULT_CI_PIPELINE_ID,
-        'LANG': 'C.UTF-8',
-    }
-
-    if 'CI_JOB_STARTED_AT' not in os.environ:
-        compose_env['CI_JOB_STARTED_AT'] = time.strftime(
-            "%Y-%m-%dT%H:%M:%SZ", time.gmtime()
-        )
-
-    if 'CI_COMMIT_SHA' not in os.environ:
-        # pylint: disable=too-many-function-args
-        compose_env['CI_COMMIT_SHA'] = (
-            sh_noout.git('rev-parse', 'HEAD').stdout.rstrip().decode('ascii')
-        )
-
-    compose_env.update(ci_vars.get_env_vars(ci_vars.ADDITIONAL_COMPOSE_VARS))
-    compose_env.update(docker_env)
-
-    docker_compose_build_files = os.environ.get(
-        'DOCKER_COMPOSE_BUILD_FILES',
-        ci_vars.DEFAULT_DOCKER_COMPOSE_BUILD_FILES,
-    )
-
-    ci_pipeline_id = compose_env['CI_PIPELINE_ID']
-
-    upx_image_registry = os.environ.get(
-        'UPX_IMAGE_REGISTRY',
-        ci_vars.DEFAULT_UPX_IMAGE_REGISTRY,
-    )
-
-    # If not set the "docker-credential-secretservice" crashes
-    # with "double free or corruption" for local builds
-    if 'DBUS_SESSION_BUS_ADDRESS' in os.environ:
-        compose_env['DBUS_SESSION_BUS_ADDRESS'] = os.environ[
-            'DBUS_SESSION_BUS_ADDRESS']
+    envs = ci_vars.get_docker_envs(BASE_DIR, pull_push=True, compose=True)
 
     for old_name in glob.iglob('.env.*.example'):
         new_name = old_name.replace('.example', '')
         shutil.copy2(old_name, new_name)
 
     sh_out.docker_compose(
-        docker_compose_build_files.split(),
+        envs['common']['DOCKER_COMPOSE_BUILD_FILES'].split(),
         'build',
         service if service else '--parallel',
-        _env=compose_env,
+        _env=envs['compose'],
     )
 
     services = ('gateway', 'server', 'web')
     if service:
         services = (service, )
     for cur_service in services:
-        image_path = '{}container-umc/umc-{}'.format(
-            upx_image_registry, cur_service
-        )
+        image_name = 'upx-umc-{}'.format(cur_service)
+
         try:
+            # push tags "build-<ci-pipeline-id>" and
+            #   "<version>-<ci-pipeline-id>"
             ci_docker.add_and_push_build_version_label_and_tag(
-                image_path, ci_pipeline_id, docker_env, pull_push_env
+                image_name,
+                envs['common']['CI_PIPELINE_ID'],
+                envs['docker'],
+                envs['pull_push'],
             )
         except ci_version.AppVersionNotFound:
             log.error('app version not found')
             return 2
-
-    # push tag "build-<ci-pipeline-id>"
-    sh_out.docker_compose(
-        docker_compose_build_files.split(),
-        'push',
-        service if service else None,
-        _env=compose_env,
-    )
+        except ci_docker.DockerPushFailed:
+            log.error('docker push failed')
+            return 3
 
     return 0
 
--- lib/ci_docker.py
+++ lib/ci_docker.py
@@ -11,6 +11,7 @@ import sh  # pylint: disable=import-error
 
 # internal imports
 from ci_log import log
+import ci_vars
 import ci_version
 
 BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
@@ -27,7 +28,12 @@ class DockerPushFailed(Exception):
     """Raised if docker pull fails"""
 
 
-def add_and_push_tag(image_name, tag, docker_env, pull_push_env):
+def add_and_push_tag(
+    image_name: str,
+    tag: str,
+    docker_env: dict,
+    pull_push_env: dict,
+):
     """Adds a tag to an image"""
     log.info('Adding tag {} to {}', tag, image_name)
     sh_out.docker.tag(image_name, tag, _env=docker_env)
@@ -39,7 +45,7 @@ def add_and_push_tag(image_name, tag, docker_env, pull_push_env):
     log.info('Done with this tag')
 
 
-def add_version_label(app_version, image_name, docker_env):
+def add_version_label(app_version: str, image_name: str, docker_env: dict):
     """Adds a version label to an image"""
     log.info('Adding version label {}', app_version)
     sh_out.docker.build(
@@ -54,26 +60,95 @@ def add_version_label(app_version, image_name, docker_env):
     log.info('Done with labeling')
 
 
+def add_and_push_build_tags(  # pylint: disable=too-many-arguments
+    build_path: str,
+    push_path: str,
+    clean_version: str,
+    ci_pipeline_id: str,
+    docker_env: dict,
+    pull_push_env: dict,
+):
+    """Add and push build tags"""
+    tag = '{}:build-{}'.format(push_path, ci_pipeline_id)
+    # push tag "build-<ci-pipeline-id>"
+    add_and_push_tag(build_path, tag, docker_env, pull_push_env)
+
+    tag = '{}:{}-{}'.format(push_path, clean_version, ci_pipeline_id)
+    # push tag "<version>-<ci-pipeline-id>"
+    add_and_push_tag(build_path, tag, docker_env, pull_push_env)
+
+
+def add_and_push_release_tags(
+    build_path: str,
+    push_path: str,
+    clean_version: str,
+    docker_env: dict,
+    pull_push_env: dict,
+):
+    """Add and push latest and version tags"""
+    tag = '{}:latest'.format(push_path)
+    # push tag "latest"
+    add_and_push_tag(build_path, tag, docker_env, pull_push_env)
+
+    tag = '{}:{}'.format(push_path, clean_version)
+    # push tag "<version>"
+    add_and_push_tag(build_path, tag, docker_env, pull_push_env)
+
+
 def add_and_push_build_version_label_and_tag(
-    image_path, ci_pipeline_id, docker_env, pull_push_env
+    image_name: str,
+    ci_pipeline_id: str,
+    docker_env: dict,
+    pull_push_env: dict,
 ):
     """Get the version, add it as a label and push it as a tag with build-id"""
-    build_path = '{}:build-{}'.format(image_path, ci_pipeline_id)
+
+    upx_image_registry = os.environ.get(
+        'UPX_IMAGE_REGISTRY',
+        ci_vars.DEFAULT_UPX_IMAGE_REGISTRY,
+    )
+    upx_image_path = '{}{}'.format(upx_image_registry, image_name)
+
+    build_path = '{}:build-{}'.format(upx_image_path, ci_pipeline_id)
     app_version = ci_version.get_app_version(build_path, docker_env)
 
     add_version_label(app_version, build_path, docker_env)
 
     clean_version = ci_version.cleanup_for_docker_tag(app_version)
-    tag = '{}:{}-{}'.format(image_path, clean_version, ci_pipeline_id)
-    # push tag "<version>-<ci-pipeline-id>"
-    add_and_push_tag(build_path, tag, docker_env, pull_push_env)
+
+    # push tag "build-<ci-pipeline-id>" and "<version>-<ci-pipeline-id>"
+    #   to univention-harbor
+    add_and_push_build_tags(
+        build_path,
+        upx_image_path,
+        clean_version,
+        ci_pipeline_id,
+        docker_env,
+        pull_push_env,
+    )
 
 
 def pull_add_push_publish_version_tag(
-    image_path, ci_pipeline_id, docker_env, pull_push_env, push_path
+    image_name: str,
+    ci_pipeline_id: str,
+    docker_env: dict,
+    pull_push_env: dict,
 ):
     """Get the version, push latest and version tags"""
-    build_path = '{}:build-{}'.format(image_path, ci_pipeline_id)
+
+    upx_image_registry = os.environ.get(
+        'UPX_IMAGE_REGISTRY',
+        ci_vars.DEFAULT_UPX_IMAGE_REGISTRY,
+    )
+    upx_image_path = '{}{}'.format(upx_image_registry, image_name)
+
+    quay_image_registry = os.environ.get(
+        'QUAY_IMAGE_REGISTRY',
+        ci_vars.DEFAULT_QUAY_IMAGE_REGISTRY,
+    )
+    quay_image_path = '{}{}'.format(quay_image_registry, image_name)
+
+    build_path = '{}:build-{}'.format(upx_image_path, ci_pipeline_id)
 
     try:
         sh_out.docker.pull(build_path, _env=pull_push_env)
@@ -82,15 +157,36 @@ def pull_add_push_publish_version_tag(
         raise DockerPullFailed from docker_pull_failed
 
     app_version = ci_version.get_app_version(build_path, docker_env)
-
-    tag = '{}:latest'.format(push_path)
-    # push tag "latest"
-    add_and_push_tag(build_path, tag, docker_env, pull_push_env)
-
     clean_version = ci_version.cleanup_for_docker_tag(app_version)
-    tag = '{}:{}'.format(push_path, clean_version)
-    # push tag "<version>"
-    add_and_push_tag(build_path, tag, docker_env, pull_push_env)
+
+    # push tag "latest" and "<version>" to univention-harbor
+    add_and_push_release_tags(
+        build_path,
+        upx_image_path,
+        clean_version,
+        docker_env,
+        pull_push_env,
+    )
+
+    # push tag "build-<ci-pipeline-id>" and "<version>-<ci-pipeline-id>"
+    #   to quay
+    add_and_push_build_tags(
+        build_path,
+        quay_image_path,
+        clean_version,
+        ci_pipeline_id,
+        docker_env,
+        pull_push_env,
+    )
+
+    # push tag "latest" and "<version>" to quay
+    add_and_push_release_tags(  # pylint: disable=too-many-function-args
+        build_path,
+        quay_image_path,
+        clean_version,
+        docker_env,
+        pull_push_env,
+    )
 
 
 # [EOF]
