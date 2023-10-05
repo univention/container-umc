8b372be95f9132f0ce80540f58ed958ce0a2647a
Valeriy Zheleznyakov <valeriy.zheleznyakov.extern@univention.de>
2023-10-05 20:52:38 +0300

    chore!: the base image is changed to ucs-base-505

--- docker/gateway/scripts/entrypoint.sh
+++ docker/gateway/scripts/entrypoint.sh
@@ -101,6 +101,4 @@ univention-config-registry commit \
 # Apache gets grumpy about PID files pre-existing
 rm -f /usr/local/apache2/logs/httpd.pid
 
-exec /usr/local/apache2/bin/httpd "$@"
-
 # [EOF]
