abef4018cef68df5337cca8aa8357e431dda5279
Thomas Kintscher <thomas.kintscher.extern@univention.de>
2024-02-27 15:55:27 +0100

    fix: integrate upstream credential handling for PostgreSQL and Memcache

--- docker/umc-server/scripts/60-self-service.sh
+++ docker/umc-server/scripts/60-self-service.sh
@@ -4,19 +4,14 @@
 
 set -euxo pipefail
 
-# ensure that self-service database password is set
-SELF_SERVICE_DB_SECRET_FILE="${SELF_SERVICE_DB_SECRET_FILE:-}"
-if [ -z "${SELF_SERVICE_DB_SECRET_FILE:-}" ]; then
-    echo "SELF_SERVICE_DB_SECRET_FILE not set"
-    exit 1
-elif [ ! -f "${SELF_SERVICE_DB_SECRET_FILE}" ]; then
-    echo "SELF_SERVICE_DB_SECRET_FILE at ${SELF_SERVICE_DB_SECRET_FILE} not found."
+# database password must be set
+if [ -z "${SELF_SERVICE_DB_SECRET:-}" ]; then 
+    echo "SELF_SERVICE_DB_SECRET is not set!"
     exit 1
 fi
 
-# if path to Memcached password file is set, the file must exist
-if [ -n "${SELF_SERVICE_MEMCACHED_SECRET_FILE:-}" ] \
-    && [ ! -f "${SELF_SERVICE_MEMCACHED_SECRET_FILE:-}" ]; then
-    echo "SELF_SERVICE_MEMCACHED_SECRET_FILE at ${SELF_SERVICE_MEMCACHED_SECRET_FILE} not found."
+# memcached password must be set
+if [ -z "${SELF_SERVICE_MEMCACHED_SECRET:-}" ]; then
+    echo "SELF_SERVICE_MEMCACHED_SECRET is not set!"
     exit 1
 fi
