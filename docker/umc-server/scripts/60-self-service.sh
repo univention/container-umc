f823ba6b5d1c2d8a90e0a72b218ca0016c233808
Johannes Bornhold <johannes.bornhold.extern@univention.de>
2024-03-08 14:07:36 +0100

    fix: Adjustments to meet the code style in linting

--- docker/umc-server/scripts/60-self-service.sh
+++ docker/umc-server/scripts/60-self-service.sh
@@ -5,7 +5,7 @@
 set -euxo pipefail
 
 # database password must be set
-if [ -z "${SELF_SERVICE_DB_SECRET:-}" ]; then 
+if [ -z "${SELF_SERVICE_DB_SECRET:-}" ]; then
     echo "SELF_SERVICE_DB_SECRET is not set!"
     exit 1
 fi
