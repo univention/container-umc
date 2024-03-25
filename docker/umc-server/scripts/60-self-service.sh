3f50d904648bb7701b0d46086afa3b32f2d06a28
Thomas Kintscher <thomas.kintscher.extern@univention.de>
2024-03-25 17:25:35 +0100

    fix: allow unauthenticated connection to memcached

--- docker/umc-server/scripts/60-self-service.sh
+++ docker/umc-server/scripts/60-self-service.sh
@@ -9,9 +9,3 @@ if [ -z "${SELF_SERVICE_DB_SECRET:-}" ]; then
     echo "SELF_SERVICE_DB_SECRET is not set!"
     exit 1
 fi
-
-# memcached password must be set
-if [ -z "${SELF_SERVICE_MEMCACHED_SECRET:-}" ]; then
-    echo "SELF_SERVICE_MEMCACHED_SECRET is not set!"
-    exit 1
-fi
