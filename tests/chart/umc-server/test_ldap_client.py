13f4bdf32130e6305a926086c46b3412fea789c1
Johannes Bornhold <johannes.bornhold.extern@univention.de>
2025-06-12 09:35:58 +0200

    feat(umc-server): Handle ldap password accoding to new secrets management

---
+++ tests/chart/umc-server/test_ldap_client.py
@@ -0,0 +1,28 @@
+# SPDX-License-Identifier: AGPL-3.0-only
+# SPDX-FileCopyrightText: 2025 Univention GmbH
+
+import pytest
+
+from univention.testing.helm.client.ldap import LdapAuth
+
+
+class TestLdapClient(LdapAuth):
+
+    secret_name = "release-name-umc-server-ldap"
+    workload_kind = "StatefulSet"
+
+    @pytest.mark.skip(reason="The UMC discovers the bind dn via UCR")
+    def test_auth_bind_dn_is_required():
+        pass
+
+    @pytest.mark.skip(reason="The UMC discovers the bind dn via UCR")
+    def test_auth_bind_dn_has_default():
+        pass
+
+    @pytest.mark.skip(reason="The UMC discovers the bind dn via UCR")
+    def test_auth_plain_values_bind_dn_is_templated():
+        pass
+
+    @pytest.mark.skip(reason="The UMC discovers the bind dn via UCR")
+    def test_auth_plain_values_provide_bind_dn():
+        pass
