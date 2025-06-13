4bb666b64a977521eef4759805b4c6b3f1af509b
Johannes Bornhold <johannes.bornhold.extern@univention.de>
2025-06-13 08:39:58 +0200

    test(umc-server): Integrate memcached tests from helm-test-harness

---
+++ tests/chart/umc-server/test_memcached.py
@@ -0,0 +1,39 @@
+# SPDX-License-Identifier: AGPL-3.0-only
+# SPDX-FileCopyrightText: 2025 Univention GmbH
+
+import pytest
+
+from univention.testing.helm.client.memcached import Auth, SecretUsageViaVolume
+
+
+class AuthSkipUsernameAndDatabase(Auth):
+
+    @pytest.mark.skip(reason="UMC uses the memcached username from UCR")
+    def test_auth_plain_values_provide_username():
+        pass
+
+    @pytest.mark.skip(reason="UMC uses the memcached username from UCR")
+    def test_auth_plain_values_username_is_templated():
+        pass
+
+    @pytest.mark.skip(reason="UMC uses the memcached username from UCR")
+    def test_auth_username_is_required():
+        pass
+
+    @pytest.mark.skip(reason="UMC uses the memcached username from UCR")
+    def test_auth_username_has_default():
+        pass
+
+
+class TestAuth(SecretUsageViaVolume, AuthSkipUsernameAndDatabase):
+
+    secret_name = "release-name-umc-server-memcached"
+    workload_kind = "StatefulSet"
+
+
+class TestAuthContainerPrepareConfig(SecretUsageViaVolume, AuthSkipUsernameAndDatabase):
+
+    secret_name = "release-name-umc-server-memcached"
+    workload_kind = "StatefulSet"
+
+    path_main_container = "spec.template.spec.initContainers[?@.name=='prepare-config']"
