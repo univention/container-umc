f9c50d74d5fb698790562cc454e394ffff088903
Carlos García-Mauriño <garcia-maurino@univention.de>
2025-11-01 10:05:50 +0100

    fix: add FOWNER capability to memcached container and adjust tests
    univention/dev/internal/team-nubus#1501

--- tests/chart/umc-server/test_memcached_authentication.py
+++ tests/chart/umc-server/test_memcached_authentication.py
@@ -1,6 +1,8 @@
 # SPDX-License-Identifier: AGPL-3.0-only
 # SPDX-FileCopyrightText: 2025 Univention GmbH
 
+import pytest
+
 from univention.testing.helm.auth_flavors.password_usage import (
     AuthPasswordUsageViaEnv, AuthPasswordUsageViaVolume)
 from univention.testing.helm.auth_flavors.secret_generation import \
@@ -19,9 +21,35 @@ class SettingsTestMemcachedSecret:
     # for tests around AuthPasswordUsageViaEnv
     sub_path_env_password = "env[?@name=='SELF_SERVICE_MEMCACHED_SECRET']"
 
+    def load_and_map(self, values):
+        result = super().load_and_map(values)
+        result.setdefault("memcached", {})
+        result["memcached"].setdefault("bundled", True)
+        result["memcached"].setdefault("auth", {})
+        result["memcached"]["auth"].setdefault("enabled", True)
+        return result
+
 
 class TestChartCreatesMemcachedSecretAsUser(SettingsTestMemcachedSecret, AuthSecretGenerationUser):
-    pass
+
+    @pytest.mark.skip(reason="The memcached password is auto-generated if not provided")
+    def test_auth_plain_values_password_is_required(self, chart):
+        pass
+
+    def test_auth_plain_values_password_is_auto_generated(self, chart):
+        values = self.load_and_map(
+            """
+            auth:
+              username: "stub-username"
+              password: null
+              existingSecret:
+                name: null
+            """)
+        result = chart.helm_template(values)
+        secret = result.get_resource(kind="Secret", name=self.secret_name)
+        password = secret.findone("stringData.password")
+        assert password is not None
+        assert password != ""
 
 
 class TestUmcServerUsesMemcachedCredentialsByVolume(SettingsTestMemcachedSecret, AuthPasswordUsageViaVolume):
