30cdddd32512a1e5e0a7ba28c4fc6afabfff895e
Marius Meschter <marius.meschter@univention.de>
2025-11-10 16:31:19 +0100

    test(helm): adjust tests for memcached secret
    Issue univention/dev/internal/team-nubus#1501

--- tests/chart/umc-server/test_memcached_authentication.py
+++ tests/chart/umc-server/test_memcached_authentication.py
@@ -1,59 +1,63 @@
 # SPDX-License-Identifier: AGPL-3.0-only
 # SPDX-FileCopyrightText: 2025 Univention GmbH
 
+from univention.testing.helm.auth_flavors.password_usage import AuthPasswordUsageViaEnv
+from univention.testing.helm.auth_flavors.secret_generation import AuthSecretGenerationOwner
+from univention.testing.helm.client.base import BaseTest
 import pytest
-
-from univention.testing.helm.auth_flavors.password_usage import (
-    AuthPasswordUsageViaEnv, AuthPasswordUsageViaVolume)
-from univention.testing.helm.auth_flavors.secret_generation import \
-    AuthSecretGenerationUser
 
 
 class SettingsTestMemcachedSecret:
     secret_name = "release-name-umc-server-memcached"
+    secret_default_key = "memcached-password"
+    path_password = "stringData.memcached-password"
     prefix_mapping = {"memcached.auth": "auth"}
 
-    # for tests around AuthPasswordUsageViaVolume
-    volume_name = "secret-memcached"
+
+class TestChartCreatesMemcachedPasswordAsOwner(
+    SettingsTestMemcachedSecret, AuthSecretGenerationOwner
+):
+    derived_password = "382c39b95d7fa1461e553afb42743caf654ad917"
+
+class TestMemcachedDeploymentUsesMemcachedPasswordViaEnv(
+    SettingsTestMemcachedSecret, AuthPasswordUsageViaEnv
+):
+    sub_path_env_password = "env[?@name=='MEMCACHED_PASSWORD']"
+    workload_name = "release-name-umc-server-memcached"
+    workload_kind = "Deployment"
+
+class TestUmcServerStatefulsetUsesMemcachedPasswordViaEnv(
+    SettingsTestMemcachedSecret, AuthPasswordUsageViaEnv
+):
+    sub_path_env_password = "env[?@name=='SELF_SERVICE_MEMCACHED_SECRET']"
     workload_name = "release-name-umc-server"
     workload_kind = "StatefulSet"
 
-    # for tests around AuthPasswordUsageViaEnv
-    sub_path_env_password = "env[?@name=='SELF_SERVICE_MEMCACHED_SECRET']"
-
-    def load_and_map(self, values):
-        result = super().load_and_map(values)
-        result.setdefault("memcached", {})
-        result["memcached"].setdefault("bundled", True)
-        result["memcached"].setdefault("auth", {})
-        result["memcached"]["auth"].setdefault("enabled", True)
-        return result
-
-
-class TestChartCreatesMemcachedSecretAsUser(SettingsTestMemcachedSecret, AuthSecretGenerationUser):
-
-    @pytest.mark.skip(reason="The memcached password is auto-generated if not provided")
-    def test_auth_plain_values_password_is_required(self, chart):
-        pass
-
-    def test_auth_plain_values_password_is_auto_generated(self, chart):
+class TestMemcachedAuthDisabled(BaseTest):
+    def test_secret_not_templated(self, chart):
         values = self.load_and_map(
             """
-            auth:
-              username: "stub-username"
-              password: null
-              existingSecret:
-                name: null
+            memcached:
+              auth:
+                enabled: false
             """)
         result = chart.helm_template(values)
-        secret = result.get_resource(kind="Secret", name=self.secret_name)
-        password = secret.findone("stringData.password")
-        assert password is not None
-        assert password != ""
 
+        with pytest.raises(LookupError):
+            result.get_resource(kind="Secret", name="release-name-umc-server-memcached")
 
-class TestUmcServerUsesMemcachedCredentialsByVolume(SettingsTestMemcachedSecret, AuthPasswordUsageViaVolume):
-    pass
+    def test_umc_memcached_secret(self, chart):
+        values = self.load_and_map(
+            """
+            memcached:
+              auth:
+                enabled: false
+            """)
+        result = chart.helm_template(values)
+        umc_memcached_secret = "env[?@name=='SELF_SERVICE_MEMCACHED_SECRET']"
 
-class TestUmcServerUsesMemcachedCredentialsByEnv(SettingsTestMemcachedSecret, AuthPasswordUsageViaEnv):
-    pass
+        umc_server = result.get_resource(kind="StatefulSet", name="release-name-umc-server")
+        container = umc_server.findone(self.path_container)
+        secret = container.findone(umc_memcached_secret)
+
+        assert secret['value'] == 'stub'
